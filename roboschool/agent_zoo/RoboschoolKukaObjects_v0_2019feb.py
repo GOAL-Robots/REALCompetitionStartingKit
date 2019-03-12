33bf88b2cd212ebb93b16b479f5631da0584eeb6
Francesco Mannella <francesco.mannella@gmail.com>
2019-03-12 11:50:57 +0100

    modified kuka-objects demo

--- roboschool/agent_zoo/RoboschoolKukaObjects_v0_2019feb.py
+++ roboschool/agent_zoo/RoboschoolKukaObjects_v0_2019feb.py
@@ -12,11 +12,12 @@ def demo_run():
     env.unwrapped.set_eyeShow(False)
 
 
-    obs = env.reset()    
     while True:
-        a = np.zeros(9) 
-        state, r, done, info_ = env.step(a)
-        still_open = env.render("human")
+        obs = env.reset()   
+        for t in range(100):
+            a = np.zeros(9) 
+            state, r, done, info_ = env.step(a)
+            still_open = env.render("human")
         
              
 if __name__=="__main__":
