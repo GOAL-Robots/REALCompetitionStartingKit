33afee6375d0f560b65bd87067727d8b404f254f
Francesco Mannella <francesco.mannella@gmail.com>
2019-04-08 05:59:11 +0200

    inertias

--- roboschool/agent_zoo/RoboschoolKukaObjects_v0_2019feb.py
+++ roboschool/agent_zoo/RoboschoolKukaObjects_v0_2019feb.py
@@ -10,11 +10,12 @@ def demo_run():
     env = gym.make("RoboschoolKuka-v1")
     env.unwrapped.set_eyeEnable(False)
     env.unwrapped.set_eyeShow(False)
+    env.unwrapped.used_objects = ["TOMATO_SOUP_CAN", "MUSTARD", "ORANGE"]
 
 
     while True:
         obs = env.reset()   
-        for t in range(100):
+        for t in range(800):
             a = np.zeros(9) 
             state, r, done, info_ = env.step(a)
             still_open = env.render("human")
